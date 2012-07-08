e296f854dc8e796dfd2194f191f2767e9fc18911
narkisr <narkisr@gmail.com>
2012-07-08 09:47:07 +0000

    adding lemur

--- plugins/env/env.plugin.zsh
+++ plugins/env/env.plugin.zsh
@@ -9,6 +9,8 @@ export JDK_HOME=$JAVA_HOME
 export VIM_HOME=~/.vim
 export UTILITIES=/home/ronen/Dropbox/automation/utilities
 export GRADLE_HOME=/usr/share/gradle-1.0/
+export LEMUR_HOME=~/lemur-1.0.1/bin
+export LEIN_JAVA_CMD=/usr/lib/jvm/java-6-sun/bin/java
 
 if which gem > /dev/null; then
   export PATH=$PATH:`gem env gemdir`/bin
@@ -19,4 +21,5 @@ then
   export PATH=$PATH:/opt/node/bin/
 fi
 
-export PATH=$PATH:$JRUBY_HOME/bin:$GOOKUP_HOME/bin:$CLOJURE_HOME/lein:$ROO_HOME/bin:$PAX_CONS_HOME/bin:$CLOJURE_HOME/repl:$JETTY_HOME/bin:$VIM_HOME/bin:$IDEA_HOME/bin:$UTILITIES:$GRADLE_HOME/bin
+
+export PATH=$PATH:$JRUBY_HOME/bin:$GOOKUP_HOME/bin:$CLOJURE_HOME/lein:$ROO_HOME/bin:$PAX_CONS_HOME/bin:$CLOJURE_HOME/repl:$JETTY_HOME/bin:$VIM_HOME/bin:$IDEA_HOME/bin:$UTILITIES:$GRADLE_HOME/bin:$LEMUR_HOME
