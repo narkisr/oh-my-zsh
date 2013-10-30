05634269e113c5181732ed35617deb327f02330f
Ronen Narkis <narkisr@gmail.com>
2013-10-30 12:50:03 +0200

    lein should use default java

--- plugins/env/env.plugin.zsh
+++ plugins/env/env.plugin.zsh
@@ -14,7 +14,7 @@ export VIM_HOME=~/.vim
 export UTILITIES=/home/ronen/Dropbox/automation/utilities
 export GRADLE_HOME=/usr/share/gradle
 export LEMUR_HOME=~/lemur-1.0.1/bin
-export LEIN_JAVA_CMD=/usr/lib/jvm/java-6-oracle/bin/java
+export LEIN_JAVA_CMD=/usr/bin/java
 
 if which gem > /dev/null; then
   export PATH=$PATH:`gem env gemdir`/bin
