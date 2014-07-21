c23f16009c8b62339e4d18e61499ea5612cf117c
narkisr <narkisr@gmail.com>
2014-07-21 11:16:13 +0300

    cleaning up

--- plugins/misc/misc.plugin.zsh
+++ plugins/misc/misc.plugin.zsh
@@ -4,55 +4,31 @@ alias bd='popd'
 
 # which process uses port
 whichport(){
-	netstat -atunp | grep $@
+  netstat -atunp | grep $@
 }
 
 findbyname(){
-	find . -name $@
+  find . -name $@
 }
 
 tunnelcouch(){
-	ssh -L5983:127.0.0.1:5984 $@
+  ssh -L5983:127.0.0.1:5984 $@
 }
 
 lsym(){
-	for i in $(find -maxdepth 1 -type l -printf '%f ')
-	do
-		echo $i '->' $(readlink $i)
-	done
+  for i in $(find -maxdepth 1 -type l -printf '%f ')
+  do
+    echo $i '->' $(readlink $i)
+  done
 }
 
 alias cp='cp -r'
 
-syn-to-cld(){
-    echo "enter pass"
-    read -s pass
-    result=`curl -X POST http://localhost:5984/_replicate -H 'Content-Type: application/json' -d "{ \"source\": \"snippets\", \"target\": \"http://narkisr:$pass@narkisr.cloudant.com/snippets\" }"`
-    echo $result | python -mjson.tool
-}
-
-syn-from-cld(){
-    result=`curl -X POST http://localhost:5984/_replicate -H 'Content-Type: application/json' -d "{ \"target\": \"http://localhost:5984/snippets\", \"source\": \"http://narkisr.cloudant.com/snippets\" }"`
-    echo $result | python -mjson.tool
-}
-
-syn-from-cone(){
-    result=`curl -X POST http://localhost:5984/_replicate -H 'Content-Type: application/json' -d "{ \"target\": \"http://localhost:5984/snipapp-restruct\", \"source\": \"http://narkisr.couchone.com/snipapp-restruct\" }"`
-    echo $result | python -mjson.tool
-}
-
-syn-to-cone(){
-    echo "enter pass"
-    read -s pass
-    result=`curl -X POST http://localhost:5984/_replicate -H 'Content-Type: application/json' -d "{ \"source\": \"snipapp-restruct\", \"target\": \"http://remote:$pass@narkisr.couchone.com/snipapp-restruct\" }"`
-    echo $result | python -mjson.tool
-}
-
 tunnel-vnc(){
-    echo "enter host"
-    read host
-    echo "use localhost:5901 in vncviewer"
-    ssh -L5901:127.0.0.1:5900 $host -p 2222
+  echo "enter host"
+  read host
+  echo "use localhost:5901 in vncviewer"
+  ssh -L5901:127.0.0.1:5900 $host -p 2222
 }
 
 # a sock proxy to remote host
@@ -75,7 +51,6 @@ max-memory(){
 disk-usage(){
   du -x . | sort -rn | more
 }
-
 
 uncommitted(){
   for gitdir in `find ./ -name .git`; 
