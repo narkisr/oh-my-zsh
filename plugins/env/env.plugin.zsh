3130b5d70ea162f0a55c6c0f39871208a2a1f7a7
ronen <narkisr@gmail.com>
2015-02-15 14:55:21 +0200

    fixing java home

--- plugins/env/env.plugin.zsh
+++ plugins/env/env.plugin.zsh
@@ -1,7 +1,9 @@
 
 # export LC_ALL=en_US.UTF-8
 
-if [ -d  /usr/lib/jvm/java-7-openjdk-amd64 ]; then
+if [ -d  /usr/lib/jvm/java-7-oracle ]; then
+  export JAVA_HOME=/usr/lib/jvm/java-7-oracle
+elif [ -d  /usr/lib/jvm/java-7-openjdk-amd64 ]; then
   export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64
 elif [ -d  /usr/lib/jvm/java-6-oracle ]; then
     export JAVA_HOME=/usr/lib/jvm/java-6-oracle
