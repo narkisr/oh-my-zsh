8ae5ade7e9f61b3c8a1952303c5a2130cffff89f
ronen <narkisr@gmail.com>
2017-12-09 00:21:10 +1100

    indent

--- plugins/git-aliases/git-aliases.plugin.zsh
+++ plugins/git-aliases/git-aliases.plugin.zsh
@@ -14,27 +14,27 @@
 # alias gcp='git cherry-pick'
 # alias gitc='git commit -a -m '
 # alias gits='git status '
-# 
-# 
+#
+#
 # #
 # # Will return the current branch name
 # # Usage example: git pull origin $(current_branch)
 # #
-# 
+#
 # function current_branch() {
 #   ref=$(git symbolic-ref HEAD 2> /dev/null) || return
 #   echo ${ref#refs/heads/}
 #   }
-# 
+#
 # # these aliases take advangate of the previous function
 # alias ggpull='git pull origin $(current_branch)'
 # alias ggpush='git push origin $(current_branch)'
 # alias ggpnp='git pull origin $(current_branch) && git push origin $(current_branch)'
-# 
+#
 function git-sub-rm(){
   git rm --cached $1
   awk -v reg="submodule \"$1\"" '$0 ~ reg {c=3}!(c&&c--)'  .gitmodules >/tmp/.gitmodules
-  cp /tmp/.gitmodules . 
+  cp /tmp/.gitmodules .
   rm -rf $1
 }
 
@@ -59,7 +59,7 @@ function git-re-attach(){
   git merge HEAD@{1}
 }
 
-# see http://tinyurl.com/aatlung  
+# see http://tinyurl.com/aatlung
 function git-unstash-file(){
   git checkout stash@{0} -- $1
 }
@@ -70,7 +70,7 @@ function gh-pages-clean(){
   rm '.gitignore'
 }
 
-# Remove multiple remote git branches 
+# Remove multiple remote git branches
 function rm-remote-branch(){
   git branch -r | awk -F/ "/\/$1/{print \$2}" | xargs -I {} git push origin :{}
 }
