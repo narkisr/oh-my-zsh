a25c0dfa2433ee33e003bb3d44482e70ca6cbd16
ronen <narkisr.com>
2018-09-24 14:33:19 +1000

    cleanup

--- plugins/env/env.plugin.zsh
+++ plugins/env/env.plugin.zsh
@@ -9,32 +9,8 @@ fi
 
 export JDK_HOME=$JAVA_HOME
 export VIM_HOME=~/.vim
+export GEM_HOME=~/.gems
 export GRADLE_HOME=/usr/share/gradle
-export LEMUR_HOME=~/lemur-1.0.1/bin
 export LEIN_JAVA_CMD=/usr/bin/java
 
-if which gem > /dev/null; then
-  export PATH=$PATH:`gem env gemdir`/bin
-fi
-
-if [ -f /opt/node/bin/node ] ;
-then
-  export PATH=$PATH:/opt/node/bin/
-fi
-
-if [ -f /usr/local/go/bin/go ] ;
-then
-  export PATH=$PATH:/usr/local/go/bin/
-fi
-
-if [ -f /opt/elixir/bin/elixir ] ;
-then
-  export PATH=$PATH:/opt/elixir/bin/
-fi
-
-if [ -d /opt/elm/ ];
-then
-  PATH=$PATH:/opt/elm/bin
-fi
-
-export PATH=$PATH:~/bin/:$JRUBY_HOME/bin:$GOOKUP_HOME/bin:$CLOJURE_HOME/lein:$ROO_HOME/bin:$PAX_CONS_HOME/bin:$CLOJURE_HOME/repl:$JETTY_HOME/bin:$VIM_HOME/bin:$IDEA_HOME/bin:$UTILITIES:$LEMUR_HOME:$GRADLE_HOME/bin/
+export PATH=$PATH:$GEM_HOME/bin/
